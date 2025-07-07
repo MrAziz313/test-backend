77f149434d6b6881ea789e96023ed8e000f0c40d
Mr Aziz <mraziz1335@gmail.com>
2025-07-07 18:17:48 +0500

    index

--- index.js
+++ index.js
@@ -3,32 +3,31 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const rateLimit = require("express-rate-limit");
-const app = express()
-const port = 3001
-
+const app = express();
+const port = 3001;
 
 app.use(
-    cors({
-      origin:
-        "https://online-islamic-institute-admin.vercel.app" ||
-        "http://localhost:3003",
-      credentials: true,
-    })
-  );
+  cors({
+    origin:
+      "https://online-islamic-institute-admin.vercel.app" ||
+      "http://localhost:3001",
+    credentials: true,
+  })
+);
 
-app.get("/",(req,res)=>{
-res.send("Hello")
-})
+app.get("/", (req, res) => {
+  res.send("Hello welcome to backend");
+});
 // Rate limiter for resend OTP
 const resendOTPLimiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 5, // Limit to 5 requests per window
-    message: "Too many OTP resend requests, please try again later.",
-  });
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5, // Limit to 5 requests per window
+  message: "Too many OTP resend requests, please try again later.",
+});
 
 mongoose
   .connect(
-      "mongodb+srv://islamic_institute:KBasUcG5v7IIKPoL@cluster0.zxklh5p.mongodb.net/islamic_institute_db?retryWrites=true&w=majority&appName=Cluster0",
+    "mongodb+srv://islamic_institute:KBasUcG5v7IIKPoL@cluster0.zxklh5p.mongodb.net/islamic_institute_db?retryWrites=true&w=majority&appName=Cluster0",
     {
       serverSelectionTimeoutMS: 5000, // 5 seconds timeout
       maxPoolSize: 10, // Limit connection pool
@@ -38,7 +37,6 @@ mongoose
   .then(() => console.log("MongoDB connected successfully"))
   .catch((err) => console.error("MongoDB connection error:", err.message));
 
-app.listen(port,()=>{
-console.log(`Serveris starting on Poart ${port}`);
-
-})
+app.listen(port, () => {
+  console.log(`Serveris starting on Poart ${port}`);
+});
